3921e59dd974822e30aee4207d01b3eeebdbaa14
mahizaman490 <mahizaman490@gmail.com>
2023-09-21 12:37:00 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import SimpleForm from './components/SimpleForm/SimpleForm'
+
+function App() {
+ 
+
+  return (
+    <>
+      <SimpleForm></SimpleForm>
+    </>
+  )
+}
+
+export default App
